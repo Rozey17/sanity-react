009a7cf1e3bfc9d1ae12c016bd54c885cba2d478
Ferrad Makosso <36968544+Rozey17@users.noreply.github.com>
2022-08-27 12:20:22 +0200

    chore: wip

--- react-app/pages/advert/[slug]/index.tsx
+++ react-app/pages/advert/[slug]/index.tsx
@@ -21,20 +21,43 @@ export default function AdvertPage({ advert }) {
         <div className="absolute inset-0">
           <div className="flex items-center justify-between px-40 py-10 text-white">
             <span>hey</span>
-            <span>hey</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-10 h-10 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
           </div>
         </div>
       </div>
       <section className="px-40 py-20 bg-gray-100">
         <div className="grid grid-cols-3 gap-5">
-          <div className="h-[500px] col-span-2">
+          <div className="col-span-2 space-y-10">
             <img
               src={advert?.image?.asset?.url}
               alt=""
-              className="object-contain w-full h-full "
+              className="object-cover w-full h-[500px] "
             />
+            <div className="p-8 bg-white"></div>
           </div>
-          <div className="">{advert?.title}</div>
+          <div className="space-y-10">
+            {advert?.title}
+
+            <div className="flex flex-col justify-between p-8 space-y-10 bg-white">
+              <p className="font-medium capitalize">ad owner</p>
+              <span className="p-5 text-white bg-red-400">
+                {advert?.contact}
+              </span>
+            </div>
+          </div>
         </div>
       </section>
     </>
