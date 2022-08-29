f889a4c9d1e268200126df8342508236e397a585
Ferrad Makosso <36968544+Rozey17@users.noreply.github.com>
2022-08-29 09:18:00 +0200

    chore: wip

--- react-app/components/map.tsx
+++ react-app/components/map.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Advert } from "./apollo-components";
+import { LocationMarkerIcon } from "@heroicons/react/outline";
+import L from "leaflet";
 
 const Map = ({ ads }: { ads: Advert[] }) => {
+  const icon = L.icon({
+    iconUrl:
+      "/images/location-marker-location-pin-location-pointer-map-locator-map-pin-svgrepo-com.svg",
+  });
+
   return (
-    <MapContainer center={[63, 89]} zoom={5} scrollWheelZoom={false}>
+    <MapContainer zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {ads?.map((item, index) => (
         <Marker
-          // icon={}
+          icon={icon}
           key={index}
           position={{
             lat: item?.location.lat,
