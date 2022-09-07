82e788460bc9f65580c6d02887fcdd2ff259e361
Ferrad Makosso <36968544+Rozey17@users.noreply.github.com>
2022-09-07 18:56:26 +0200

    chore: wip

--- react-app/pages/test.tsx
+++ react-app/pages/test.tsx
@@ -1,8 +1,5 @@
 import dynamic from "next/dynamic";
-import { CheckboxComponent } from "../components/checkboxComponent";
-const Map = dynamic(() => import("../components/map"), {
-  ssr: false,
-});
+
 
 export default function Home() {
   return "hello";
