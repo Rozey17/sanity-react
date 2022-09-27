d509874bb3537070232f41d507891d7c5c63a68e
Ferrad Makosso <36968544+Rozey17@users.noreply.github.com>
2022-09-27 09:12:55 +0200

    chore: wip

--- react-app/pages/_document.tsx
+++ react-app/pages/_document.tsx
@@ -8,30 +8,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json" />
           <link
-            rel="apple-touch-icon"
-            href="/images/android-chrome-192x192.png"
-          ></link>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&&display=optional"
             rel="stylesheet"
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          ></link>
-          {/* <link
-            rel="stylesheet"
-            href="https://unpkg.com/leaflet@^1.8.0/dist/leaflet.css"
-            integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          /> */}
-          {/* <script
-            src="https://unpkg.com/leaflet@^1.8.0/dist/leaflet.js"
-            integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-          ></script> */}
+
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
